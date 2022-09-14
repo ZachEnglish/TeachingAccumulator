adbf34b57e605614d32e3c6983fdc461243d98ba
Zach English <englishz@gmail.com>
2022-09-13 22:43:17 -0700

    Playing with first GUI.

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 # This is a sample Python script.
+import PySimpleGUI as sg
 
+layout = [
+    [sg.Text("First Name"), sg.Input(key="FIRST_NAME")],
+    [sg.Text("Last Name"), sg.Input(key="LAST_NAME")],
+    [sg.Button("Popup"), sg.Button("Read"), sg.Exit()],
+]
+
+window = sg.Window("My first GUI", layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == "Exit":
+        break
+    if event == "Read":
+        print(event, values)
+    if event == "Popup":
+        print("So you want a popup?")
+
+window.close()
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -10,7 +29,7 @@ def print_hi(name):
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#if __name__ == '__main__':
+#    print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
